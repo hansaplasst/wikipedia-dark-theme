9e6ad6df8d92a2a8db56cde223e065ecc42977e8
hansaplasst <forum@logic-q.nl>
2026-08-31 01:26:59 +0200

    Add Wikipedia Dark Theme userscript
    This userscript automatically selects the dark theme on Wikipedia and hides the appearance menu after selection.

---
+++ wikipedia-dark-theme.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         Wikipedia Dark Theme
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @description  Auto Enable Wikipedia Dark Theme
+// @author       Hansaplast
+// @match        *://*.wikipedia.org/*
+// @match        *://*.mediawiki.org/*
+// @match        *://*.wikidata.org/*
+// @match        *://*.wikisource.org/*
+// @icon         https://en.wikipedia.org/static/favicon/wikipedia.ico
+// @run-at       document-idle
+// @grant        none
+// @license      GNU GPLv3
+// @downloadURL  https://update.greasyfork.org/scripts/570311/Wikipedia%20Dark%20Theme.user.js
+// @updateURL    https://update.greasyfork.org/scripts/570311/Wikipedia%20Dark%20Theme.meta.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const automaticSelector =
+        'input[name="skin-client-pref-skin-theme-group"][value="night"]';
+
+    const hideSelector =
+        'button[aria-label="Hide Appearance"]';
+
+    let automaticClicked = false;
+    let attempts = 0;
+    const maxAttempts = 100;
+
+    const timer = setInterval(() => {
+        attempts++;
+
+        // Select the Dark Theme
+        if (!automaticClicked) {
+            const automatic = document.querySelector(automaticSelector);
+
+            if (automatic) {
+                if (!automatic.checked) {
+                    automatic.click();
+                    console.log('Wikipedia: Automatic Theme Selected.');
+                }
+
+                automaticClicked = true;
+            }
+        }
+
+        // Hide Appearance menu
+        if (automaticClicked) {
+            const hideButton = document.querySelector(hideSelector);
+
+            if (hideButton) {
+                hideButton.click();
+                console.log('Wikipedia: Hide Appearance menu');
+                clearInterval(timer);
+            }
+        }
+
+        if (attempts >= maxAttempts) {
+            clearInterval(timer);
+            console.log('Wikipedia: reached timeout.');
+        }
+    }, 100);
+})();
